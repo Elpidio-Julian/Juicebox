473eeb2f1ddcb9620c33208787ca619e82bbf69f
Elpidio-Julian <elpidiojulian@gmail.com>
2022-09-25 20:45:36 -0500

    part 2 finished

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const client = new Client('postgres://localhost:5432/juicebox-dev')
 
 async function getAllUsers() {
     const { rows } = await client.query(
-        `SELECT id, username 
+        `SELECT id, username, name, location, active 
       FROM users;
     `);
 
@@ -14,20 +14,151 @@ async function getAllUsers() {
 }
 
 
-async function createUser({ username, password }) {
+async function createUser({
+    username,
+    password,
+    name,
+    location
+}) {
     try {
-        const { rows } = await client.query(`
-      INSERT INTO users(username, password)
-      VALUES ($1, $2);
+        const { rows: [ user ] } = await client.query(`
+      INSERT INTO users(username, password, name, location)
+      VALUES ($1, $2, $3, $4)
       ON CONFLICT (username) DO NOTHING
       RETURNING *;
-    `, [username, password]);
+    `, [username, password, name, location]);
 
-        return rows;
+        return user;
     } catch (error) {
         throw error;
     }
 }
+
+async function updateUser(id, fields = {}) {
+    // build the set string
+    const setString = Object.keys(fields).map(
+      (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+  
+    // return early if this is called without fields
+    if (setString.length === 0) {
+      return;
+    }
+  
+    try {
+      const {rows: [ user ]} = await client.query(`
+        UPDATE users
+        SET ${ setString }
+        WHERE id=${ id }
+        RETURNING *;
+      `, Object.values(fields));
+  
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function createPost({
+    authorId,
+    title,
+    content
+  }) {
+    try {
+      const { rows: [ post ] } = await client.query(`
+        INSERT INTO posts("authorId", title, content) 
+        VALUES($1, $2, $3)
+        RETURNING *;
+      `, [authorId, title, content]);
+  
+      return post;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function updatePost(id, fields = {}) {
+    // build the set string
+    const setString = Object.keys(fields).map(
+      (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+  
+    // return early if this is called without fields
+    if (setString.length === 0) {
+      return;
+    }
+  
+    try {
+      const { rows: [ post ] } = await client.query(`
+        UPDATE posts
+        SET ${ setString }
+        WHERE id=${ id }
+        RETURNING *;
+      `, Object.values(fields));
+  
+      return post;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getAllPosts() {
+    try {
+        const { rows } = await client.query(
+            `SELECT "authorId", title, content, active 
+          FROM posts;
+        `);
+    
+        return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getPostsByUser(userId) {
+    try {
+      const { rows } = await client.query(`
+        SELECT * FROM posts
+        WHERE "authorId"=${ userId };
+      `);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  
+  async function getUserById(userId) {
+    try {
+        const { rows: [user] } = await client.query(`
+        SELECT * FROM users
+        WHERE id=${userId};
+    `)
+    if(!user) return null;
+    delete user.password
+    const posts = await getPostsByUser(userId)
+    user.posts = posts
+    return user;
+    } catch (error) {
+        throw error;
+    }
+
+    // first get the user (NOTE: Remember the query returns 
+      // (1) an object that contains 
+      // (2) a `rows` array that (in this case) will contain 
+      // (3) one object, which is our user.
+    // if it doesn't exist (if there are no `rows` or `rows.length`), return null
+  
+    // if it does:
+    // delete the 'password' key from the returned object
+    // get their posts (use getPostsByUser)
+    // then add the posts to the user object with key 'posts'
+    // return the user object
+  }
+
+
+
 
 
 
@@ -35,4 +166,10 @@ module.exports = {
     client,
     getAllUsers,
     createUser,
+    updateUser,
+    createPost,
+    updatePost,
+    getAllPosts,
+    getPostsByUser,
+    getUserById
 }
